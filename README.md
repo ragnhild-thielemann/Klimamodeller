3125fd5f4e70a5b8e7e6861bd0206275d98fcbb0
Ragnhild Thielemann <ragnhild.thi@gmail.com>
2026-03-01 08:02:22 +0100

    ja

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Klimamodeller
 Beskrivelse av klimamodeller i MAT1020
 
+Vi tar utgangpunkt i notatet ([Flath & Co: "Energy Balance Models"](https://www.uio.no/studier/emner/matnat/math/MAT1020/v26/mat1020flatharticle.pdf))
 
 
 # Modelering av klimamodeller
--- første_fil.py
+++ første_fil.py
@@ -8,3 +8,12 @@ import matplotlib.pyplot as plt
 
 T_verdier = np.linspace(220,310,1000) #tar temperaturer i kelvin fra 220 K - 310 K (tilsvarer intervallet fra -53 C til 37 C
 
+
+
+
+
+plt.xlabel("Temperatur(K)")
+plt.ylabel("Energitettheten")
+plt.title("Drivhuseffekten på jordens klimabalanse")
+plt.grid(True)
+plt.show()
